5a95f1f9785f317b2ceea555a5b7274e0f2a9d41
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-10-24 20:01:50 -0400

    :fire:

--- app/page.js
+++ app/page.js
@@ -1,113 +1,147 @@
-import Image from 'next/image'
+import Image from "next/image";
+import A from "./assets/-9.png";
+import B from "./assets/dam.png";
+import C from "./assets/-8.png";
+const Nav = () => {
+  return (
+    <div className="px-12 text-secondary py-6 bg-lightGreen flex items-center justify-between">
+      <img src="" alt="logo" />
+      <div className="flex items-center gap-4">
+        <a href="#Home">Home</a>
+        <a href="#About">About</a>
+        <a href="#Services">Services</a>
+      </div>
+      <a
+        className="px-[22px] py-2 bg-primary text-white font-bold rounded-full"
+        href="#"
+      >
+        Book an oppointment
+      </a>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <>
+      <Nav />
+      <section
+        id="#Home"
+        className="bg-lightGreen w-full flex flex-col px-36 md:px-20 max-sm:px-12 py-8"
+      >
+        <div className="bg-secondary flex gap-4 max-[1000px]:flex-col items-center px-12 max-[1000px]:pt-12  rounded-[40px] justify-evenly">
+          <div className="max-w-[500px]">
+            <h1 className="text-white text-5xl font-bold mt-4">
+              SmileGuard with Our Exceptional Dental Services!
+            </h1>
+            <p className="text-white my-6">
+              Our skilled team of experienced dentists is dedicated to enhancing
+              your smile's health and beauty
+            </p>
+            <a
+              className="px-[40px] text-[15px] py-[10px] bg-primary text-white font-bold rounded-full"
+              href="#"
+            >
+              Learn more
+            </a>
+          </div>
+          <Image src={B} className="rounded-lg w-[450px] h-[500px]" />
+        </div>
+        <div className="flex flex-row-reverse max-[1000px]:flex-col max-[1000px]:items-center items-center justify-evenly relative">
+          <div className="w-[450px] bg-white shadow-lg  mt-[-80px] rounded-[20px] px-8 py-8">
+            <h2 className="text-secondary text-2xl font-bold mb-2">
+              Book an appointment
+            </h2>
+            <p className="text-secondary text-sm">
+              Get your dental health back on track with us
+            </p>
+            <div className="flex mt-12 flex-col gap-6">
+              <input
+                type="text"
+                placeholder="Your Name"
+                className="border px-4 py-[10px] text-gray-400 border-gray-300 rounded-md"
+              />
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="border px-4 py-[10px] text-gray-400 border-gray-300 rounded-md"
+              />
+              <input
+                type="text"
+                placeholder="Phone Number"
+                className="border px-4 py-[10px] text-gray-400 border-gray-300 rounded-md"
+              />
+              <div className="flex justify-between">
+                <input
+                  type="date"
+                  placeholder="Date"
+                  className="border px-4 py-[10px] text-gray-400 border-gray-300 rounded-md"
+                />
+                <input
+                  type="time"
+                  placeholder="Time"
+                  className="border px-4 py-[10px] text-gray-400 border-gray-300 rounded-md"
+                />
+              </div>
+              <textarea
+                placeholder="Message"
+                className="border border-gray-300 px-4 py-[10px] w-full h-[200px] rounded-md"
+              ></textarea>
+              <a
+                href="#"
+                className="w-full text-center text-white rounded-[20px] font-bold py-3 bg-primary"
+              >
+                Book Now
+              </a>
+            </div>
+          </div>
+          <div className="flex max-w-[700px]  max-sm:flex-col mx-8 items-center justify-center mt-16">
+            <div className="flex flex-col">
+              <p className="text-primary font-bold mb-4">WHAT WE DO</p>
+              <h2 className="text-secondary text-4xl mb-4 font-bold">
+                Provide genuine dental care
+              </h2>
+              <p className="text-secondary mb-8">
+                We offer a comprehensive range of dental services to help you
+                achieve optimal oral health and a beatiful smile. Our team of
+                experienced
+              </p>
+              <a
+                href="#"
+                className="py-2 max-w-[150px] text-center font-bold rounded-full border-2 border-secondary text-secondary"
+              >
+                Learn More
+              </a>
+            </div>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src={A}
+              alt="4"
+              className="ml-6 w-[400px] h-[300px] max-sm:ml-0 max-sm:mt-8 max-sm:h-[280px] max-sm:w-[280px]"
             />
+          </div>
+        </div>
+      </section>
+      <section className="flex max-sm:flex-col items-center py-12 justify-evenly px-36 md:px-20 max-sm:px-12">
+        <div className="max-w-[600px]">
+          <p className="text-primary font-bold mb-2">INTORDUCTION</p>
+          <h2 className="text-secondary font-bold text-4xl mb-6">
+            Trusted dental services for you and our family .
+          </h2>
+          <p className="text-secondary mb-12">
+            At our dental practice, we offer a comprehensive range of dental
+            services to help you achieve optimal oral health and a beatiful
+            smile. Our team of experienced and friendly dental professionals is
+            committed to providing core in a comfortable.
+          </p>
+          <a
+            href="#"
+            className="py-2 px-8 text-center font-bold rounded-full border-2 border-secondary text-secondary"
+          >
+            Meet doctors
           </a>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <Image src={C} className="max-sm:mt-8" />
+      </section>
+    </>
+  );
 }
